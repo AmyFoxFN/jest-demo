bac68b5fae1e1bc8bc6f7d3bda87b5a450f1c84c
AmyFoxFN <funanamy@gmail.com>
2020-02-02 22:33:13 +0800

    feat: snapshot

--- test/3d-render-e2e.test.ts
+++ test/3d-render-e2e.test.ts
@@ -2,3 +2,15 @@ test('should be titled correctly', async () => {
   await page.goto('http://localhost:8000/')
   await expect(page.title()).resolves.toMatch('Jest Demo')
 })
+
+test('should be render correctly', async () => {
+  await page.goto('http://localhost:8000/')
+  page.screenshot({ path: 'test/snapshots/3d-render-1.png' })
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve()
+    }, 1000)
+  })
+
+  await expect(page.title()).resolves.toMatch('Jest Demo')
+})
--- test/puppeteer.test.ts
+++ test/puppeteer.test.ts
@@ -1,4 +1,4 @@
-test('baidu.com should be titled correctly', async () => {
+test.skip('baidu.com should be titled correctly', async () => {
   await page.goto('https://baidu.com')
   await expect(page.title()).resolves.toMatch('百度一下，你就知道')
 })
