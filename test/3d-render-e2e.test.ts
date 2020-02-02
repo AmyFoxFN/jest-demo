5861a0bc35da6c4667c4ce160f8ffca0b2b540fb
AmyFoxFN <funanamy@gmail.com>
2020-02-02 22:36:11 +0800

    chore

--- test/3d-render-e2e.test.ts
+++ test/3d-render-e2e.test.ts
@@ -3,14 +3,7 @@ test('should be titled correctly', async () => {
   await expect(page.title()).resolves.toMatch('Jest Demo')
 })
 
-test('should be render correctly', async () => {
+test('should be render correctly when loaded', async () => {
   await page.goto('http://localhost:8000/')
-  page.screenshot({ path: 'test/snapshots/loaded-render.png' })
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, 1000)
-  })
-
-  await expect(page.title()).resolves.toMatch('Jest Demo')
+  await page.screenshot({ path: 'test/snapshots/loaded-render.png' })
 })
