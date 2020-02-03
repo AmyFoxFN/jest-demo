da92b74e1ed5344a4ba3e9ebc1ef6a790bd568cf
AmyFoxFN <funanamy@gmail.com>
2020-02-03 16:42:27 +0800

    feat: image

--- test/3d-render-e2e.test.ts
+++ test/3d-render-e2e.test.ts
@@ -1,3 +1,6 @@
+// @todo declare ts
+// @ts-nocheck
+
 test('should be titled correctly', async () => {
   await page.goto('http://localhost:8000/')
   await expect(page.title()).resolves.toMatch('Jest Demo')
@@ -5,5 +8,7 @@ test('should be titled correctly', async () => {
 
 test('should be render correctly when loaded', async () => {
   await page.goto('http://localhost:8000/')
-  await page.screenshot({ path: 'test/snapshots/loaded-render.png' })
+  const image = await page.screenshot()
+
+  expect(image).toMatchImageSnapshot()
 })
